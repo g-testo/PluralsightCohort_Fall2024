4a1c49267a5302a02fe62e895e3394aa80a85b4b
Greg <gt@gregory.com>
2024-10-08 16:25:22 -0400

    Added candies.csv file

--- java-development/workbook-3/CandyExample_ReadWrite/src/main/java/com/ps/Main.java
+++ java-development/workbook-3/CandyExample_ReadWrite/src/main/java/com/ps/Main.java
@@ -1,36 +1,37 @@
 package com.ps;
 
 public class Main {
+    static Candy[] inventory = new Candy[100];
     public static void main(String[] args) {
         // Do we understand the problem?
-        // Creating an app to keep track of candy in a store
-        // Class for Candy
-            // Properties
-                // String name, float price, String flavor, short quantity
-            // Methods
-                // toString, getters and setters
-        // Keep track of candy
-        // Read candy from a file
-        // Create candy and write it to a file
+            // Creating an app to keep track of candy in a store
+            // Class for Candy
+                // Properties
+                    // String name, float price, String flavor, short quantity
+                // Methods
+                    // toString, getters and setters
+            // Keep track of candy
+            // Read candy from a file
+            // Create candy and write it to a file
 
         // Create a plan
 
-        // Create the class for Candy
-        // Create a static variable for tracking inventory of candy
-        // Create a file with initial inventory
+            // Create the class for Candy
+            // Create a static variable for tracking inventory of candy
+            // Create a file with initial inventory
 
-        // Read candy from file
-        // Loop every line of candy
-            // add each piece of candy to the inventory variable
+            // Read candy from file
+            // Loop every line of candy
+                // add each piece of candy to the inventory variable
 
-        // Add candy
-            // Add the new candy to the file(Append)
+            // Add candy
+                // Add the new candy to the file(Append)
 
-        // Show all candy (Alphabetically)
-        // Show all candy (Flavor)
-        // Show all candy (Price)
+            // Show all candy (Alphabetically)
+            // Show all candy (Flavor)
+            // Show all candy (Price)
 
-        // Search(To do)
+            // Search(To do)
 
         // Do it
         // Refactor
