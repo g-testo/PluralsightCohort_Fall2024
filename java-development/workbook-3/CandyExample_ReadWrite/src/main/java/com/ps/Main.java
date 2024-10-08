fd9c601d76eae27b8874e28426fab463cae3d920
Greg <gt@gregory.com>
2024-10-08 16:01:27 -0400

    added candy example

---
+++ java-development/workbook-3/CandyExample_ReadWrite/src/main/java/com/ps/Main.java
@@ -0,0 +1,39 @@
+package com.ps;
+
+public class Main {
+    public static void main(String[] args) {
+        // Do we understand the problem?
+        // Creating an app to keep track of candy in a store
+        // Class for Candy
+            // Properties
+                // String name, float price, String flavor, short quantity
+            // Methods
+                // toString, getters and setters
+        // Keep track of candy
+        // Read candy from a file
+        // Create candy and write it to a file
+
+        // Create a plan
+
+        // Create the class for Candy
+        // Create a static variable for tracking inventory of candy
+        // Create a file with initial inventory
+
+        // Read candy from file
+        // Loop every line of candy
+            // add each piece of candy to the inventory variable
+
+        // Add candy
+            // Add the new candy to the file(Append)
+
+        // Show all candy (Alphabetically)
+        // Show all candy (Flavor)
+        // Show all candy (Price)
+
+        // Search(To do)
+
+        // Do it
+        // Refactor
+
+    }
+}
--- java-development/workbook-3/Playground/src/main/java/com/ps/Main.java
+++ java-development/workbook-3/Playground/src/main/java/com/ps/Main.java
@@ -1,35 +1,50 @@
 package com.ps;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // Steps to import from a file:
+//        // Steps to import from a file:
+//
+//            // Create file to import
+//                // Done!
+//        try{
+//            // Create instance of BufferedReader(With instance of FileReader)
+//            BufferedReader bufferedReader = new BufferedReader(new FileReader("students.txt"));
+//
+//            // Create variable to store the first line
+//            String header = bufferedReader.readLine();
+//            System.out.println(header);
+//
+//            String input;
+//
+//            // Loop through each line until we reach the end of the file (While)
+//            while((input = bufferedReader.readLine()) != null){
+//                // do stuff
+//                System.out.println(input);
+//            }
+//            // Close the BufferedReader instance
+//            bufferedReader.close();
+//
+//        } catch(Exception e){
+//            e.printStackTrace();
+//        }
 
-            // Create file to import
-                // Done!
-        try{
-            // Create instance of BufferedReader(With instance of FileReader)
-            BufferedReader bufferedReader = new BufferedReader(new FileReader("students.txt"));
 
-            // Create variable to store the first line
-            String header = bufferedReader.readLine();
-            System.out.println(header);
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("test.txt", true));
 
-            String input;
+            bufferedWriter.write("Hello\n");
+            bufferedWriter.write("World");
 
-            // Loop through each line until we reach the end of the file (While)
-            while((input = bufferedReader.readLine()) != null){
-                // do stuff
-                System.out.println(input);
-            }
-            // Close the BufferedReader instance
-            bufferedReader.close();
+            bufferedWriter.close();
 
-        } catch(Exception e){
+        } catch (Exception e){
             e.printStackTrace();
         }
     }
