f76af7f29175e07502f790501893a31b6bd0c4b7
Greg <gt@gregory.com>
2024-10-08 14:54:20 -0400

    add code to import file

--- java-development/workbook-3/Playground/src/main/java/com/ps/Main.java
+++ java-development/workbook-3/Playground/src/main/java/com/ps/Main.java
@@ -1,5 +1,8 @@
 package com.ps;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,14 +10,27 @@ public class Main {
         // Steps to import from a file:
 
             // Create file to import
+                // Done!
+        try{
             // Create instance of BufferedReader(With instance of FileReader)
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("students.txt"));
 
             // Create variable to store the first line
+            String header = bufferedReader.readLine();
+            System.out.println(header);
+
+            String input;
 
             // Loop through each line until we reach the end of the file (While)
-                // Do stuff
-
-
+            while((input = bufferedReader.readLine()) != null){
+                // do stuff
+                System.out.println(input);
+            }
             // Close the BufferedReader instance
+            bufferedReader.close();
+
+        } catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
