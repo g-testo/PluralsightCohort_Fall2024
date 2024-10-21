49f31aff8cfbb3bf26bbb9e9646470b3af645791
Greg <gt@gregory.com>
2024-10-21 16:58:41 -0400

    added hotel class

---
+++ java-development/workbooks/workbook-4/Hotel_App_OOP/src/main/java/com/ps/Hotel.java
@@ -0,0 +1,47 @@
+package com.ps;
+
+public class Hotel {
+    private String name;
+    private int numberOfSuites;
+    private int numberOfRooms;
+    private int bookedSuites;
+    private int bookedRooms;
+
+    public Hotel(String name, int numberOfSuites, int numberOfRooms) {
+        this.name = name;
+        this.numberOfSuites = numberOfSuites;
+        this.numberOfRooms = numberOfRooms;
+        this.bookedSuites = 0;
+        this.bookedRooms = 0;
+    }
+
+    public Hotel(String name, int numberOfSuites, int numberOfRooms, int bookedSuites, int bookedRooms) {
+        this.name = name;
+        this.numberOfSuites = numberOfSuites;
+        this.numberOfRooms = numberOfRooms;
+        this.bookedSuites = bookedSuites;
+        this.bookedRooms = bookedRooms;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfSuites() {
+        return numberOfSuites;
+    }
+
+    public int getNumberOfRooms() {
+        return numberOfRooms;
+    }
+
+    public int getBookedSuites() {
+        return bookedSuites;
+    }
+
+    public int getBookedRooms() {
+        return bookedRooms;
+    }
+
+
+}
