b342dd0a69c4f39f7c34535b53e24fbdcfb9629d
Greg <gt@gregory.com>
2024-10-23 15:35:26 -0400

    Added backjack exercise

---
+++ java-development/workbooks/workbook-4/BlackjackExercise/src/main/java/com/ps/Card.java
@@ -0,0 +1,54 @@
+package com.ps;
+
+public class Card {
+    private String suit;
+    private String value;
+    private boolean isFaceUp;
+
+    public Card(String suit, String value) {
+        this.suit = suit;
+        this.value = value;
+        this.isFaceUp = false;
+    }
+
+    public String getSuit() {
+        // only return the suit if the card is face up
+        if (isFaceUp) {
+            return suit;
+        } else {
+            return "#";
+        }
+    }
+
+    public String getValue() {
+        // only return the value if the card is face up
+        if (isFaceUp) {
+            // this is the string value of the card
+            // i.e. A, K, Q, J, 10, 9 ...
+            return value;
+        } else {
+            return "#";
+        }
+    }
+
+    public int getPointValue() {
+        // only return the value if the card is face up
+        if (isFaceUp) {
+            // determine point value and return it
+            // A = 11
+            // K, Q, J = 10
+            // all numeric cards are equal to their face value
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+
+    public boolean isFaceUp() {
+        return isFaceUp;
+    }
+
+    public void flip() {
+        isFaceUp = !isFaceUp;
+    }
+}
