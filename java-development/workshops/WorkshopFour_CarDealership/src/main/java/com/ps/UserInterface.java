56d4a987c5def1199232a9008c817e06d95df8b6
Greg <gt@gregory.com>
2024-10-24 15:36:32 -0400

    added Workshop4 workshop

---
+++ java-development/workshops/WorkshopFour_CarDealership/src/main/java/com/ps/UserInterface.java
@@ -0,0 +1,16 @@
+package com.ps;
+
+public class UserInterface {
+    // private static Dealership dealership;
+
+    public static void init(){
+        System.out.println("Init method");
+        //Loading the dealership and inventory from a file
+    }
+    public static void display(){
+        init();
+        System.out.println("Load the menu");
+        // Load the menu
+    }
+
+}
