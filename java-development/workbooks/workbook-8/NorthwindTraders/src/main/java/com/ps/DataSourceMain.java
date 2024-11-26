0145c82e8589ffe757b5afb4a6e4496f602ab617
Greg <gt@gregory.com>
2024-11-26 11:36:16 -0500

    added data source example

--- java-development/workbooks/workbook-8/NorthwindTraders/src/main/java/com/ps/DataSourceMain.java
+++ java-development/workbooks/workbook-8/NorthwindTraders/src/main/java/com/ps/DataSourceMain.java
@@ -2,22 +2,40 @@ package com.ps;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 
-import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class DataSourceMain {
     public static void main(String[] args) {
-        if (args.length != 2) {
-            System.out.println(
-                    "Application needs two arguments to run: " +
-                            "java com.hca.jdbc.UsingDriverManager <username> " + "<password>");
-            System.exit(1);
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl("jdbc:mysql://localhost:3306/northwind");
+        basicDataSource.setUsername(args[0]);
+        basicDataSource.setPassword(args[1]);
+
+        int productId = 1;
+
+        String query = "SELECT * FROM products WHERE productid=?;";
+        try(
+                Connection connection = basicDataSource.getConnection();
+                PreparedStatement statement = connection.prepareStatement(query);
+        ) {
+            statement.setInt(1, productId);
+            try(
+                    ResultSet results = statement.executeQuery();
+            ) {
+                System.out.println("Id\tName\t\t\t\t\t\t\t\tPrice\tStock");
+                System.out.println("------------------");
+                while (results.next()) {
+                    System.out.printf("%-4s %-35s %-8.2f %-7d%n",
+                            results.getString("productid"),
+                            results.getString("productname"),
+                            results.getFloat("unitprice"),
+                            results.getInt("unitsinstock"));
+                }
+            }
+        } catch (Exception e){
+            e.printStackTrace();
         }
-        String username = args[0];
-        String password = args[1];
-
-        DataSource dataSource = new BasicDataSource();
-
-
-
     }
 }
